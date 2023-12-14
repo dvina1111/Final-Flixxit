31f6b1b5e2b1cdfc5a0245637643b155e43033df
dvina1111 <73433211+dvina1111@users.noreply.github.com>
2023-12-14 14:51:45 +0530

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import {
-  createUserWithEmailAndPassword,
-  onAuthStateChanged,
-} from "firebase/auth";
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+  } from "firebase/auth";
 import React,  {useEffect, useState} from 'react';
 import styled from "styled-components";
 import BackgroundImage from '../components/BackgroundImage';
@@ -13,121 +13,131 @@ import { Link, useNavigate } from 'react-router-dom';
 
 
 export default function SignUp() {
-  const [showPassword, setShowPassword] = useState(false);
-  const navigate = useNavigate();
-  const [formValues, setFormValues] = useState({
-    email: "",
-    password: "",
-  });
+    const [showPassword, setShowPassword] = useState(false);
+    const navigate = useNavigate();
+    const [formValues, setFormValues] = useState({
+      email: "",
+      password: "",
+    });
+    
   
-
-  const handleSignIn = async () => {
-      try {
-        const { email, password } = formValues;
-        await createUserWithEmailAndPassword(firebaseAuth, email, password);
-      } catch (error) {
-        console.log(error.code);
-      }
-    };
-
-  useEffect(() => {
-      const unsubscribe = onAuthStateChanged(firebaseAuth, (currentUser) => {
-        if (currentUser) {
-          navigate('/Flixxit');
+    const handleSignIn = async () => {
+        try {
+          const { email, password } = formValues;
+          await createUserWithEmailAndPassword(firebaseAuth, email, password);
+        } catch (error) {
+          console.log(error.code);
         }
-        });
-      return () => {
-          unsubscribe();
-        };
-      }, [navigate]);
+      };
 
-  return (
-    <Container $showPassword={showPassword}>
-      <Link to="/login">Already have an account? Login</Link>
-      <Link to="/Flixxit">Go to home</Link>
-      <BackgroundImage />
-      <div className="content">
-        <Header login />
-        <div className="body flex column a-center j-center">
-          <div className="text flex column">
-            <h1>Unlimited movies, TV shows and more.</h1>
-            <h4>Watch anywhere. Cancel anytime.</h4>
-            <h6>
-              Ready to watch? Enter your email to create or restart membership.
-            </h6>
-          </div>
-          <div className="form">
-            <input
-              type="email"
-              placeholder="Email address"
-              name="email"
-              value={formValues.email}
-              onChange={(e) =>
-                setFormValues({
-                  ...formValues,
-                  [e.target.name]: e.target.value,
-                })
-              }
-            />
-            {showPassword && (
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(firebaseAuth, (currentUser) => {
+          if (currentUser) {
+            navigate('/Flixxit');
+          }
+          });
+        return () => {
+            unsubscribe();
+          };
+        }, [navigate]);
+  
+    return (
+      <Container $showPassword={showPassword}>
+        <Link to="/login">Already have an account? Login</Link>
+        <Link to="/Flixxit">Go to home</Link>
+        <BackgroundImage />
+        <div className="content">
+          <Header login />
+          <div className="body flex column a-center j-center">
+            <div className="text flex column">
+              <h1>Unlimited movies, TV shows and more.</h1>
+              <h4>Watch anywhere. Cancel anytime.</h4>
+              <h6>
+                Ready to watch? Enter your email to create or restart membership.
+              </h6>
+            </div>
+            <div className="form">
               <input
-                type="password"
-                placeholder="Password"
-                name="password"
-                value={formValues.password}
+                type="email"
+                placeholder="Email address"
+                name="email"
+                value={formValues.email}
                 onChange={(e) =>
                   setFormValues({
                     ...formValues,
                     [e.target.name]: e.target.value,
                   })
                 }
-               
-                
               />
-            )}
-            {!showPassword && (
-              <button onClick={() => setShowPassword(true)}>Get Started</button>
-            )}
+              {showPassword && (
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={formValues.password}
+                  onChange={(e) =>
+                    setFormValues({
+                      ...formValues,
+                      [e.target.name]: e.target.value,
+                    })
+                  }
+                 
+                  
+                />
+              )}
+              {!showPassword && (
+                <button onClick={() => setShowPassword(true)}>Get Started</button>
+              )}
+            </div>
+            {showPassword && <button onClick={handleSignIn}>Log In</button>}
           </div>
-          {showPassword && <button onClick={handleSignIn}>Log In</button>}
         </div>
-      </div>
-    </Container>
-  );
-}
+      </Container>
+    );
+  }
 
 const Container = styled.div`
-position: relative;
-.content {
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: rgba(0, 0, 0, 0.5);
-  height: 100vh;
-  width: 100vw;
-  display: grid;
-  grid-template-rows: 15vh 85vh;
-  .body {
-    gap: 1rem;
-    .text {
+  position: relative;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-rows: 15vh 85vh;
+    .body {
       gap: 1rem;
-      text-align: center;
-      font-size: 2rem;
-      h1 {
-        padding: 0 25rem;
+      .text {
+        gap: 1rem;
+        text-align: center;
+        font-size: 2rem;
+        h1 {
+          padding: 0 25rem;
+        }
       }
-    }
-    .form {
-      display: grid;
-      width: 60%;
-      input {
-        color: black;
-        border: none;
-        padding: 1.5rem;
-        font-size: 1.2rem;
-        border: 1px solid black;
-        &:focus {
-          outline: none;
+      .form {
+        display: grid;
+        width: 60%;
+        input {
+          color: black;
+          border: none;
+          padding: 1.5rem;
+          font-size: 1.2rem;
+          border: 1px solid black;
+          &:focus {
+            outline: none;
+          }
+        }
+        button {
+          padding: 0.5rem 1rem;
+          background-color: #e50914;
+          border: none;
+          cursor: pointer;
+          color: white;
+          font-weight: bolder;
+          font-size: 1.05rem;
         }
       }
       button {
@@ -136,20 +146,11 @@ position: relative;
         border: none;
         cursor: pointer;
         color: white;
+        border-radius: 0.2rem;
         font-weight: bolder;
         font-size: 1.05rem;
       }
     }
-    button {
-      padding: 0.5rem 1rem;
-      background-color: #e50914;
-      border: none;
-      cursor: pointer;
-      color: white;
-      border-radius: 0.2rem;
-      font-weight: bolder;
-      font-size: 1.05rem;
-    }
   }
-}
 `;
+      
